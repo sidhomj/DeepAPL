c2119d172344d821998e6f795f7a54688d9250e9
JW <sidhomj@gmail.com>
2020-03-22 01:07:00 -0400

    initial_commit

--- scripts/Model Performance.py
+++ scripts/Model Performance.py
@@ -18,6 +18,11 @@ with open('Cell_Preds.pkl','rb') as f:
 with open('Cell_Masks.pkl','rb') as f:
     DAPL.w = pickle.load(f)
 
+DAPL.Cell_Pred['n'] = 1
+agg = DAPL.Cell_Pred.groupby(['Patient']).agg({'Label':'first','n':'sum'})
+sns.swarmplot(data=agg,x='Label',y='n')
+plt.ylabel('Number of Cells')
+
 #Cell Performance
 plt.figure()
 plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
@@ -40,6 +45,24 @@ sns.violinplot(data=DAPL.Cell_Pred,x='Label',y='APL',hue='Cell_Type',hue_order=o
 
 #Sample Level Performance
 DAPL.Sample_Summary(Load_Prev_Data=True)
+plt.figure()
+plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+y_test = np.asarray(DAPL.sample_summary['Label']) == 'APL'
+y_pred = np.asarray(DAPL.sample_summary['APL'])
+roc_score = roc_auc_score(y_test,y_pred)
+fpr, tpr, th = roc_curve(y_test, y_pred)
+id = 'APL'
+plt.plot(fpr, tpr, lw=2, label='%s (area = %0.4f)' % (id, roc_score))
+plt.legend(loc="lower right")
+
+#Sample Level Performance with samples >= 10 cells
+DAPL.Sample_Summary(Load_Prev_Data=True)
+keep = np.array(list(agg[agg['n']>=10].index))
+DAPL.sample_summary = DAPL.sample_summary[DAPL.sample_summary.index.isin(keep)]
 plt.figure()
 plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
 plt.xlim([0.0, 1.0])
