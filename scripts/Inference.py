9a7dc93052636238794cd8ed43e5ba5dc5d14a57
JW <sidhomj@gmail.com>
2020-05-14 01:06:56 -0400

    update

--- scripts/Inference.py
+++ scripts/Inference.py
@@ -9,23 +9,21 @@ warnings.filterwarnings('ignore')
 import matplotlib.pyplot as plt
 
 data = 'load_data'
-name = 'discovery_blast_2018'
-name = 'ig_test_discovery'
-name_out = 'validation_blast_2018'
-name_out = 'ig_test_validation'
+name = 'discovery_model'
+name_out = 'validation_model'
 num_mc = 100
 
+#Load Trained Model
 gpu = 1
 os.environ["CUDA DEVICE ORDER"] = 'PCI_BUS_ID'
 os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu)
 DAPL = DeepAPL_SC(data,gpu)
 DAPL.Import_Data(directory=None, Load_Prev_Data=True)
 
+#Get data from Validation Cohort
 df_meta = pd.read_csv('../Data/master.csv')
 df_meta['Date of Diagnosis'] = df_meta['Date of Diagnosis'].astype('datetime64[ns]')
 df_meta.sort_values(by='Date of Diagnosis',inplace=True)
-# df_meta =df_meta[df_meta['Date of Diagnosis']>= '2018-01-01']
-# df_meta = df_meta[df_meta['Cohort']=='Discovery']
 df_meta = df_meta[df_meta['Cohort']=='Validation']
 
 idx_samples_keep = np.isin(DAPL.patients,df_meta['JH Number'])
@@ -45,6 +43,8 @@ DAPL_train.lb = LabelEncoder().fit(DAPL_train.labels)
 DAPL_train.Y = DAPL_train.lb.transform(DAPL_train.labels)
 DAPL_train.Y = OneHotEncoder(sparse=False).fit_transform(DAPL_train.Y.reshape(-1,1))
 DAPL_train.predicted = np.zeros((len(DAPL_train.Y), len(DAPL_train.lb.classes_)))
+
+#Conduct Inference over ensemble of trained models on discovery cohort
 DAPL_train.Ensemble_Inference()
 DAPL_train.counts = np.ones_like(DAPL_train.predicted)*num_mc
 DAPL_train.Get_Cell_Predicted()
@@ -53,4 +53,3 @@ with open(name_out+'.pkl','wb') as f:
     pickle.dump([DAPL_train.Cell_Pred,DAPL_train.w,DAPL_train.imgs,
                 DAPL_train.patients,DAPL_train.cell_type,DAPL_train.files,DAPL_train.smears,
                 DAPL_train.labels,DAPL_train.Y,DAPL_train.predicted,DAPL_train.lb],f,protocol=4)
-check=1
